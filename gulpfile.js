d478b0c6edf1b30219a980a9ee66266233d6f8d6
Andrea <lauralme96@gmail.com>
2022-01-11 17:05:40 -0500

    1101

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,10 @@
 const { src, dest, watch } = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
+const plumber = require('gulp-plumber');
 
 function css(done){
     src('src/scss/**/*.scss') //Indentificar el archivo .scss a compilar
+        .pipe( plumber() )
         .pipe( sass() ) //Compilarlo
         .pipe( dest('build/css') )  //ALmacenarla en el disco duro
     done();
@@ -10,7 +12,7 @@ function css(done){
 
 function dev(done){
 
-    watch('src/scss/app.scss', css);
+    watch('src/scss/**/*.scss', css);
 
     done();
 }
